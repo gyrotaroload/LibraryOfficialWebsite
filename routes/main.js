fb18725ed95c5b1abdda12607eea1e210f58567d
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-11-14 18:49:58 +0800

    Book input front end setup

--- routes/main.js
+++ routes/main.js
@@ -36,6 +36,20 @@ router.get('/', ensureAuthenticated, function (req, res, next) {
 //});
 //});
 
+router.get('/add_periodical', ensureAuthenticated, function (req, res, next) {
+    res.render('add_periodical', {
+        title: 'add_periodical',
+        var_jade_user_info_name: `${req.user.name}`,
+        var_jade_user_info_username: `${req.user.username}`,
+        var_jade_user_info_profileimage: `${req.user.profileimage}`,
+        var_use_old_jquery: true,
+        var_jade_err_msg_show: false,
+        var_jade_error_msg_gui_text_1: "X",
+        var_jade_error_msg_gui_text_2: "X",
+
+    });
+});
+
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
