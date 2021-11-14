06e89f4b1140039c65b96ed78a16d19aedc0ab01
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-11-14 22:17:18 +0800

    frion end with red lable

--- public/periodical/index.js
+++ public/periodical/index.js
@@ -5,3 +5,19 @@ document.getElementById('submit').addEventListener('click', function () {
         //empty
     });
 });
+
+function DOCE(e) {
+    //console.log(e.srcElement.value);
+    var selectvalue = e.srcElement.value;
+    if (selectvalue) {
+        var selindex = parseInt(selectvalue);
+        var ops = e.target.getElementsByTagName('option');
+        for (let index = 0; index < ops.length; index++) {
+            const element = ops[index];
+            if (element.value === String(selindex)) {
+                document.getElementById('STAT').value = element.innerText;
+            }
+        }
+        //document.getElementById('STAT').value = e.target.getElementsByTagName('option')[parseInt(selectvalue)].innerText;
+    }
+}
