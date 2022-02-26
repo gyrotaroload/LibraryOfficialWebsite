238ae1e1ea37ed44d79c6f815b3548de77d5a292
andythebreaker <e24074724@mail.ncku.edu.tw>
2022-02-26 17:39:40 +0800

    fix css problems

--- public/jumbotron/js/qrcodeMapButton.js
+++ public/jumbotron/js/qrcodeMapButton.js
@@ -8,22 +8,62 @@ const config = { attributes: true, childList: true, subtree: true };
 
 // Create an observer instance linked to the callback function
 const observer = new MutationObserver(function (mutationsList, observer) {
-    if (document.querySelector('.ui.basic.massive.image.label img')) {
-        if (document.querySelector('.ui.basic.massive.image.label img').src) {
-            var qrcodeimg = document.querySelector('.ui.basic.massive.image.label img').src;
-            var map_button_img = document.createElement("img");
-            map_button_img.src = qrcodeimg;
-            document.getElementById('qrcode').innerHTML = "";
-            document.getElementById('qrcode').appendChild(map_button_img);
-            var map_button_inner_html = document.createElement("i");
-            map_button_inner_html.classList.add('map');
-            map_button_inner_html.classList.add('marker');
-            map_button_inner_html.classList.add('alternate');
-            map_button_inner_html.classList.add('icon');
-            document.getElementById('qrcode').appendChild(map_button_inner_html);
+    console.log(1);
+    if (document.querySelector('#qrcode img')) {
+        console.log(1);
 
+        if (document.querySelector('#qrcode img').src) {
+
+            var qrcodeimg = document.querySelector('#qrcode img').src;
+
+            var map_button_img = document.createElement("img");
+
+            map_button_img.src = qrcodeimg;
+
+            document.getElementById('qrcode').innerHTML = "";
+
+            document.getElementById('qrcode').appendChild(map_button_img);
+
+            var map_button_inner_html = document.createElement("i");
+
+            map_button_inner_html.classList.add('map');
+
+            map_button_inner_html.classList.add('marker');
+
+            map_button_inner_html.classList.add('alternate');
+
+            map_button_inner_html.classList.add('icon');
+
+            document.getElementById('qrcode').appendChild(map_button_inner_html);
             document.getElementById('qrcode').innerHTML += " 地圖 &raquo;";
             observer.disconnect();
+        }else{
+            document.querySelector('#qrcode canvas').toBlob(function(blob){ 
+                var qrcodeimg = window.URL.createObjectURL(blob);
+    
+            var map_button_img = document.createElement("img");
+
+            map_button_img.src = qrcodeimg;
+
+            document.getElementById('qrcode').innerHTML = "";
+
+            document.getElementById('qrcode').appendChild(map_button_img);
+
+            var map_button_inner_html = document.createElement("i");
+
+            map_button_inner_html.classList.add('map');
+
+            map_button_inner_html.classList.add('marker');
+
+            map_button_inner_html.classList.add('alternate');
+
+            map_button_inner_html.classList.add('icon');
+
+            document.getElementById('qrcode').appendChild(map_button_inner_html);
+            document.getElementById('qrcode').innerHTML += " 地圖 &raquo;";
+            observer.disconnect();
+            }, 'image/jpeg', 0.95);
+            
         }
     }
 });
