f0b8bff66c0d13a50a29a1f54dbf934ab7178856
gyrotaroload <zuangtirung@protonmail.com>
2022-03-27 19:11:16 +0000

    整理www程式碼

--- bin/wwwcopy.mjs
+++ bin/wwwcopy.mjs
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+'use strict';
 
 /**
  * Random security code generation (please put it at the beginning of the file)
@@ -29,16 +30,8 @@ import ws_msg_income_obj from "../models/esm/keywordscopy.mjs";
 /**
  * Get port from environment and store in Express.
  */
-
 var port = normalizePort(process.env.PORT || '18787');
 app.set('port', port);
-
-
-////////////////////////////
-//const SocketServer = require('ws').Server;
-//////////////////////////////////ws//////////////////////////////////////////
-//app.myLibrary(ws_msg_income_obj);
-//var server = http.createServer(app);
 
 /**
  * Random Security Code Generation Calculation
@@ -54,64 +47,33 @@ async function verifyJWT(jwt) {
 }
 
 /**
- * Create HTTP server.
- */
-//const server = http.createServer(app);
-
-/**
- * Create WS server.
- */
-//let wss = new WebSocket.Server({ clientTracking: true, noServer: true });
-//({ server: server });
-
-
-
-
-'use strict';
-
+* Express and WebSocket listening on the same port
+* https://stackoverflow.com/questions/34808925/express-and-websocket-listening-on-the-same-port/34838031#34838031
+*/
 let WSServer = require('ws').Server;
 let server = require('http').createServer();
 
 // Create web socket server on top of a regular http server
 let wss = new WSServer({
-
   server: server
 });
 
 // Also mount the app here
 server.on('request', app);
+
 /**
  * START ALL server.
  */
 server.listen(port, function () {
-  console.log(`amazing duo-server listening on 80`);
+  //Create a table
+  const success_MSG = [
+    { success_MSG: `http/ws start success on port ${port}` }];
+
+  //print
+  printTable(success_MSG);
 });
 
-//and also mount the express app ... something like ..
-//server.on('request', app); //.. something like that
-///////////////////WS area////////////////////////////
-//var ws_go = null;
-//指定開啟的 port
-//const PORT13030 = process.env.wsPORT || 13030;
-/*app.myLibrary = {
-  port: PORT13030,
-  app: express(),
-  after_the_goods_enter_the_warehouse_the_processing_modulefunction: function (ws_go_mjs) {
-    ws_go = ws_go_mjs;
-  }
-};*/
 
-//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
-//const server 
-//app.myLibrary.app.listen(PORT13030, () => console.log(`WS Listening on ${PORT13030}`));
-
-//將 express 交給 SocketServer 開啟 WebSocket 的服務
-/*const wss = new SocketServer({ server ,
-  verifyClient: (info, done) => {
-    req.session.passport.user(info.req, {}, () => {
-      done(info.req.session)
-    })
-  }});*/
 //當 WebSocket 從外部連結時執行
 wss.on('connection', ws => {
 
@@ -164,76 +126,6 @@ wss.on('connection', ws => {
               .then(decoded => {
                 ws_debug_BruteForceTest(`verifyJWT(tm)            .then(decoded => {`);
                 if (tf && decoded.stuff === ht) {
-              /*//TODO iat/exp
-
-              ///////////////////////////////////////////////////////////pdf/////////////////////////////////////
-              pdf.create(decoded.stuff).toBuffer(function (err, buffer) {
-                //console.log('This is a buffer:', b64a.encode(buffer));
-                if (!err && Buffer.isBuffer(buffer)) {
-                  try {
-                    ws.send(b64a.encode(buffer));
-                  } catch (error_of_ws) {
-                    console.log(error_of_ws);
-                  }
-                } else {
-                  try {
-                    ws.send('pdf||');
-                  } catch (error_of_ws) {
-                    console.log(error_of_ws);
-                  }
-                  console.log(err);
-                }
-              });
-              ////////////////////////////////////////////////////////////////////////////////////////////////
-
-              var pt = parse(ht);
-              translate(pt.text, { to: 'en' }).then(res => {//TODO 翻譯功能要換掉 https://github.com/shikar/NODE_GOOGLE_TRANSLATE/issues/7
-                //TypeError: Cannot read property '1' of null
-                //Url: https://translate.google.com/_/TranslateWebserverUi/data/batchexecute?rpcids=MkEWBc&f.sid=9118077551142346777&bl=boq_translate-webserver_20220206.16_p0&hl=en-US&soc-app=1&soc-platform=1&soc-device=1&_reqid=4040&rt=c
-                //at D:~/code\LibraryOfficialWebsiteNew\LibraryOfficialWebsite\node_modules\translate-google\index.js:179:15
-                //at processTicksAndRejections (internal/process/task_queues.js:93:5) {
-                //code: 'BAD_NETWORK'
-                //}
-                debug(res);
-                if (res) {
-
-                  kw(res, ka => {
-                    translate(ka, { to: 'zh-tw' }).then(restw => {
-                      debug(restw);
-                      var JSON_stringify_restw_items = restw.Keywords.concat(restw.Keyphrases);
-                      debug(JSON_stringify_restw_items);
-                      var JSON_stringify_restw_unique = [...new Set(JSON_stringify_restw_items)];
-                      try {
-                        ws.send(JSON.stringify(JSON_stringify_restw_unique));
-                      } catch (error_of_ws) {
-                        console.log(error_of_ws);
-                      }
-
-                    }).catch(errtw => {
-                      console.log(errtw);
-                      try {
-                        ws.send('解析失敗!');
-                      } catch (error_of_ws) {
-                        console.log(error_of_ws);
-                      }
-                    })
-                  })
-                } else {
-                  try {
-                    ws.send('解析失敗!');
-                  } catch (error_of_ws) {
-                    console.log(error_of_ws);
-                  }
-                }
-              }).catch(err => {
-                console.log(err);
-                try {
-                  ws.send('解析失敗!');
-                } catch (error_of_ws) {
-                  console.log(error_of_ws);
-                }
-              })
-            */} else {
                   ws_debug_BruteForceTest(decoded.stuff);
                   try {
                     ws.send(ws_go(decoded.stuff));
@@ -268,25 +160,16 @@ wss.on('connection', ws => {
         console.log(error_of_ws);
       }
     }
-  })
+  });
 
   //當 WebSocket 的連線關閉時執行
   ws.on('close', () => {
-    console.log('Close connected')
-  })
-})
-//////////////////////////////////ws//////////////////////////////////////////
+    console.log('Close connected');
+  });
+});
 
 
-
-
-//server.listen(port);
-
-/**
- //////////////// Listen on provided port, on all network interfaces.
- */
 server.on('error', onError);
-//server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -335,15 +218,3 @@ function onError(error) {
       throw error;
   }
 }
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-/*function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}*/
